8e9bf200ce47f8277a012bb65131a8e3255a6360
Bonehead <47313866+B0N3head@users.noreply.github.com>
2022-11-16 02:02:59 +1000

    Fix video time for SearchYoutubeVideoAsync

--- src/YTSearch.NET.TestConsole/Program.cs
+++ src/YTSearch.NET.TestConsole/Program.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace YTSearch.NET.TestConsole
-{   
+{
     class TestConsole
     {
         static async Task Main(string[] args)
@@ -12,25 +13,41 @@ namespace YTSearch.NET.TestConsole
             var client = new YouTubeSearchClient();
 
             //var a = (await client.SearchYoutubeAsync("never gonna give you up")).Results.First().Url;
-            var result = (await client.GetVideoMetadataAsync(new Uri("https://www.youtube.com/watch?v=yXQViqx6GMY"))).Result;
+            var _metadata_test = (await client.GetVideoMetadataAsync(new Uri("https://www.youtube.com/watch?v=yXQViqx6GMY"))).Result;
 
             //Debugger.Break();
 
-            Console.WriteLine(result.Author); // MariahCareyVEVO
-            Console.WriteLine(result.Category); // Music
-            Console.WriteLine(result.IsCrawlable); // True
-            Console.WriteLine(result.IsFamilyFriendly); // True
-            Console.WriteLine(result.IsLiveContent); // False
-            Console.WriteLine(result.IsPrivate); // False
-            Console.WriteLine(result.IsRatingEnabled); // True
-            Console.WriteLine(result.IsUnlisted); // False
-            Console.WriteLine(result.Length); // 00:03:55
-            Console.WriteLine(result.PublishedDate); // 23/11/2009 12:00:00 am
-            Console.WriteLine(result.UploadedDate); // 23/11/2009 12:00:00 am
-            Console.WriteLine(result.Title); // Mariah Carey - All I Want For Christmas Is You (Official Video)
-            Console.WriteLine(result.Url); // https://www.youtube.com/watch?v=yXQViqx6GMY
-            Console.WriteLine(result.VideoId); // yXQViqx6GMY
-            Console.WriteLine(result.Views); // 731599447
+            Console.WriteLine(_metadata_test.Author);           // MariahCareyVEVO
+            Console.WriteLine(_metadata_test.Category);         // Music
+            Console.WriteLine(_metadata_test.IsCrawlable);      // True
+            Console.WriteLine(_metadata_test.IsFamilyFriendly); // True
+            Console.WriteLine(_metadata_test.IsLiveContent);    // False
+            Console.WriteLine(_metadata_test.IsPrivate);        // False
+            Console.WriteLine(_metadata_test.IsRatingEnabled);  // True
+            Console.WriteLine(_metadata_test.IsUnlisted);       // False
+            Console.WriteLine(_metadata_test.Length);           // 00:03:55
+            Console.WriteLine(_metadata_test.PublishedDate);    // 23/11/2009 12:00:00 am
+            Console.WriteLine(_metadata_test.UploadedDate);     // 23/11/2009 12:00:00 am
+            Console.WriteLine(_metadata_test.Title);            // Mariah Carey - All I Want For Christmas Is You (Official Video)
+            Console.WriteLine(_metadata_test.Url);              // https://www.youtube.com/watch?v=yXQViqx6GMY
+            Console.WriteLine(_metadata_test.VideoId);          // yXQViqx6GMY
+            Console.WriteLine(_metadata_test.Views);            // 731599447
+            Console.ReadKey();
+
+            // Should be the second song in the list, depends on Youtube tho and whats most popular
+            var _video_test = (await client.SearchYoutubeVideoAsync("Mariah Carey - All I Want For Christmas Is You (Official Video)")).Results;
+
+            foreach (SearchedYouTubeVideo _vid in _video_test)
+            {
+                Console.WriteLine(Environment.NewLine);
+                Console.WriteLine(_vid.Author);           // MariahCarey
+                Console.WriteLine(_vid.Length);           // 00:03:55
+                Console.WriteLine(_vid.Title);            // Mariah Carey - All I Want For Christmas Is You (Official Video)
+                Console.WriteLine(_vid.Url);              // https://www.youtube.com/watch?v=yXQViqx6GMY
+                Console.WriteLine(_vid.VideoId);          // yXQViqx6GMY
+                Console.WriteLine(_vid.Views);            // 731599447
+                Console.ReadKey();
+            }
         }
     }
 }
